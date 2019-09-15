85bedbd56d2515830070c67277c316b2b48c9694
bass-2000 <bass-2000@yandex.ru>
2019-09-15 20:10:04 +0300

    initial commit. first Controller

---
+++ src/main/java/com/bass2000/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.bass2000.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.bass2000.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
